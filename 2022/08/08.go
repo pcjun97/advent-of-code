51fac5aefd93ad0cf4ef9633164f6bd6cf922398
Chi Jun <pcjun97@gmail.com>
2022-12-10 01:54:21 +0800

    Refactor 2022/08

--- 2022/08/08.go
+++ 2022/08/08.go
@@ -9,100 +9,190 @@ import (
 
 func main() {
 	input := aoc.ReadInput()
-	treesInput := strings.Split(input, "\n")
-	trees := make([][]*Tree, len(treesInput))
+	tg := NewTreeGrid(input)
 
-	for i, line := range treesInput {
-		trees[i] = make([]*Tree, len(line))
+	fmt.Println(len(tg.VisibleTrees()))
+	fmt.Println(tg.HighestScoreTree().ScenicScore())
+}
+
+type TreeGrid [][]*Tree
+
+func NewTreeGrid(s string) TreeGrid {
+	lines := strings.Split(s, "\n")
+
+	row := len(lines)
+	col := len(lines[0])
+
+	tg := make(TreeGrid, len(lines))
+
+	for i, line := range lines {
+		tg[i] = make([]*Tree, len(line))
 		for j, h := range line {
 			height := int(h - '0')
-			t := NewTree(height)
-
-			if i > 0 {
-				t.SetNeightbour(UP, trees[i-1][j])
-			}
-
-			if j > 0 {
-				t.SetNeightbour(LEFT, trees[i][j-1])
-			}
-
-			trees[i][j] = t
+			tg[i][j] = NewTree(j, i, height)
 		}
 	}
 
-	max := 0
-	sum := 0
-	for i := 0; i < len(trees); i++ {
-		for j := 0; j < len(trees[i]); j++ {
-			if trees[i][j].Visible() {
-				sum++
+	var max int
+	if col > row {
+		max = col
+	} else {
+		max = row
+	}
+
+	var tr [4]Tracker
+	var t [4]*Tree
+
+	for i := 0; i < max; i++ {
+		for k := 0; k < 4; k++ {
+			tr[k] = NewTracker()
+		}
+
+		for j := 0; j < max; j++ {
+			for k := 0; k < 4; k++ {
+				t[k] = nil
 			}
-			if score := trees[i][j].ScenicScore(); score > max {
-				max = score
+
+			if i < row && j < col {
+				t[0] = tg[i][j]
+			}
+
+			if i < row && max-j-1 < col {
+				t[1] = tg[i][max-j-1]
+			}
+
+			if j < row && i < col {
+				t[2] = tg[j][i]
+			}
+
+			if max-j-1 < row && i < col {
+				t[3] = tg[max-j-1][i]
+			}
+
+			for k := 0; k < 4; k++ {
+				if t[k] == nil {
+					continue
+				}
+				bt := tr[k].BlockingTree(t[k])
+				if bt == nil {
+					t[k].Visibility[k] = true
+					t[k].Score[k] = j
+				} else {
+					t[k].Visibility[k] = false
+					t[k].Score[k] = t[k].DistanceX(bt) + t[k].DistanceY(bt)
+				}
+				tr[k].Add(t[k])
 			}
 		}
 	}
 
-	fmt.Println(sum)
-	fmt.Println(max)
+	return tg
 }
 
-type Direction int
+func (tg TreeGrid) VisibleTrees() []*Tree {
+	v := []*Tree{}
+	for i := 0; i < len(tg); i++ {
+		for j := 0; j < len(tg[i]); j++ {
+			if tg[i][j].Visible() {
+				v = append(v, tg[i][j])
+			}
+		}
+	}
+	return v
+}
 
-const (
-	UP Direction = iota
-	LEFT
-	DOWN
-	RIGHT
-)
+func (tg TreeGrid) HighestScoreTree() *Tree {
+	t := (*Tree)(nil)
+	for i := 0; i < len(tg); i++ {
+		for j := 0; j < len(tg[i]); j++ {
+			if t == nil || tg[i][j].ScenicScore() > t.ScenicScore() {
+				t = tg[i][j]
+			}
+		}
+	}
+	return t
+}
 
 type Tree struct {
-	height     int
-	neighbours [4]*Tree
+	X          int
+	Y          int
+	Height     int
+	Visibility [4]bool
+	Score      [4]int
 }
 
-func NewTree(height int) *Tree {
+func NewTree(x, y, height int) *Tree {
 	t := Tree{
-		height: height,
+		X:      x,
+		Y:      y,
+		Height: height,
 	}
 
 	return &t
 }
 
 func (t *Tree) Visible() bool {
-	for i, n := range t.neighbours {
-		visible := true
-		for n != nil {
-			if n.height >= t.height {
-				visible = false
-				break
-			}
-			n = n.neighbours[i]
-		}
-		if visible {
+	for _, v := range t.Visibility {
+		if v {
 			return true
 		}
 	}
+
 	return false
 }
 
 func (t *Tree) ScenicScore() int {
 	score := 1
-	for direction, n := range t.neighbours {
-		i := 0
-		for n != nil && n.height < t.height {
-			i++
-			if n.height >= t.height {
-				break
-			}
-			n = n.neighbours[direction]
-		}
-		score *= i
+
+	for _, s := range t.Score {
+		score *= s
 	}
+
 	return score
 }
 
-func (t *Tree) SetNeightbour(direction Direction, neighbour *Tree) {
-	t.neighbours[direction] = neighbour
-	neighbour.neighbours[(direction+2)%4] = t
+func (t *Tree) DistanceX(other *Tree) int {
+	d := t.X - other.X
+	if d < 0 {
+		return -d
+	}
+	return d
+}
+
+func (t *Tree) DistanceY(other *Tree) int {
+	d := t.Y - other.Y
+	if d < 0 {
+		return -d
+	}
+	return d
+}
+
+type Tracker map[*Tree]bool
+
+func NewTracker() Tracker {
+	tr := make(Tracker)
+	return tr
+}
+
+func (tr Tracker) Add(t *Tree) {
+	for o := range tr {
+		if o.Height <= t.Height {
+			delete(tr, o)
+		}
+	}
+	tr[t] = true
+}
+
+func (tr Tracker) BlockingTree(t *Tree) *Tree {
+	bt := (*Tree)(nil)
+	for o := range tr {
+		if o.Height >= t.Height {
+			if bt == nil {
+				bt = o
+			} else if o.Height < bt.Height {
+				bt = o
+			}
+		}
+	}
+	return bt
 }
